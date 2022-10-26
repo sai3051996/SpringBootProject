94739e3857a79545a7e440e8764e5f37be4160ef
Saikumar.kondawar <Saikumar.kondawar@CITHYDLTP-CRT10.CaprusIT.local>
2022-10-26 17:46:16 +0530

    SpringBootAdded

---
+++ src/main/java/com/caprus/runner/SampleRunner.java
@@ -0,0 +1,12 @@
+package com.caprus.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SampleRunner implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Sample Runner...");
+	}
+}
